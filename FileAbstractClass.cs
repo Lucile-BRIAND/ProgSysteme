4b024f1e2f5a39aa11b986eb756bd859168fd026
danykhdr <danykhadhar@gmail.com>
2022-01-27 08:55:57 +0100

    classe abstraite

---
+++ FileAbstractClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projet
+{
+   abstract class FileAbstractClass
+    {
+        public abstract void InitFile();
+        public abstract void CloseFile();
+
+    }
+}
