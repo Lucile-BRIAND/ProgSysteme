059537e140f494a416b59ed792e08cb0a2a989de
danykhdr <danykhadhar@gmail.com>
2022-01-27 09:42:28 +0100

    ExistingJob class skeleton

---
+++ Skeleton/Appli_V1/Model/ExistingJob.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace projet
+{
+    class ExistingJob : FileAbstractClass
+    {
+
+        private StreamWriter file;
+        private static ExistingJob jobInstance = null;
+        private string sourcePath;
+        private string targetPath;
+        private string jobName;
+        private string jobType;
+
+        private ExistingJob()
+        {
+
+        }
+
+        public static ExistingJob GetInstance
+        {
+            get
+            {
+                if (jobInstance == null)
+                {
+                    jobInstance = new ExistingJob();
+                }
+                return jobInstance;
+            }
+        }
+
+        public override void InitFile()
+        {
+
+
+        }
+        public override void CloseFile()
+        {
+
+        }
+
+        public void WriteExistingJobs()
+        {
+
+        }
+        public void RemoveExistingJobs()
+        {
+
+        }
+    }
+}
