c73b759160e432ecab11130f9b14d6db63a81fb8
Victor <victor.monaque@viacesi.fr>
2022-01-27 12:09:23 +0100

    StatusLogFile.cs updated -> comments + init() and close() deleted + WriteStatusLogMessage() update

--- Skeleton/Appli_V1/Model/StatusLogFile.cs
+++ Skeleton/Appli_V1/Model/StatusLogFile.cs
@@ -2,42 +2,59 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+
 
 namespace Appli_V1.Controllers
 {
     class StatusLogFile : FileAbstractClass
     {
         private StreamWriter file;
-        private static StatusLogFile statusInstance = null;
+        private static List<StatusLogFile> statusInstance = null;
 
+        // new string
+        private string mFileName;
+
+        //the private constructor is only accessible from this class
         private StatusLogFile()
-        {
+        {}
 
-        }
-
+        //The unique function to use theStatusLogFile object
         public static StatusLogFile GetInstance
         {
+            
             get {
                 if (statusInstance == null)
                 {
-                    statusInstance = new StatusLogFile();
+                    statusInstance = new List<StatusLogFile>();
+
                 }
                 return statusInstance;
             }
         }
 
-        public override void InitFile()
+        public void WriteStatusLogMessage( string jobName, string jobType, string sourcePath, string targetPath, string State, int TotalFilesToCopy, int TotalFilesSize, int NbFilesLeftToDo)
         {
+            statusInstance.GetInstance;
+            statusInstance.Add(new StatusLogFile()
+            {
+                Name = jobName,
+                Type = jobType,
+                sourcePath = sourcePath,
+                targetPath = targetPath,
+                state = State,
+                totalFilesToCopy = TotalFilesToCopy,
+                totalFilesSize = TotalFilesSize,
+                nbFilesLeftToDo = NbFilesLeftToDo,
+            });
 
+            string json = JsonSerializer.Serialize(statusInstance);
+            File.WriteAllText("StatusLogFile.json", json);
 
         }
-        public override void CloseFile()
-        {
-
-        }
-
-        public void WriteStatusLogMessage()
-        {
 
         }
 
