db4b3abbb1c6465927291210259a517425f0b793
Lucile-BRIAND <lucilebriand.pro@gmail.com>
2022-01-27 12:02:02 +0100

    LogFile.cs updated -> comments + init() and close() deleted + write() updated

--- Skeleton/Appli_V1/Model/LogFile.cs
+++ Skeleton/Appli_V1/Model/LogFile.cs
@@ -2,43 +2,58 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Appli_V1.Controllers
 {
     class LogFile : FileAbstractClass
     {
-        private static LogFile logInstance = null;
-        private StreamWriter file;
+        //THIS CLASS IS A SINGLETON
+        //
+        //Private attributes
+        private static List<LogFile> logInstance = null; //default unique instance
+        private StreamWriter file; //file object we could write in
+        private LogFile key; //key object for the json file
         
-        private  LogFile()
+        //Private constructor, only accessible from this class
+        private LogFile()
         {
 
         }
+
+        //Getting the unique instance of this class
         public static LogFile GetInstance
         {
             get
             {
                 if (logInstance == null)
                 {
-                    logInstance = new LogFile();
+                    logInstance = new List<LogFile>(); //creating a list
                 }
                 return logInstance;
             }  
             
         }
-        public void WriteLogMessage()
+
+        //Writing content in the log file
+        public void WriteLogMessage(string jobName, string sourcePath, string targetPath, int fileSize)
         {
+            //Adding values to the json keys
+            logInstance.Add(new LogFile()
+            {
+                Name = jobName,
+                FileSource = sourcePath,
+                FileTarget = targetPath,
+                FileSize = fileSize,
+                FileTransferTime = null, //waiting to find the proper method
+                Date = null //timestamp for the beginning of the job + waiting to find the proper method
+            });
 
+            //Reserializing the json file and writing
+            string json = JsonSerializer.Serialize(logInstance);
+            file.Write("DailyLog.json", json);
         }
-        public override void InitFile()
-        {
-
-        }
-        public override void CloseFile()
-        {
-
-        }
-
 
     }
 }
