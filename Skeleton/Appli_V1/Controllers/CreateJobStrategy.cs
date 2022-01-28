39ea9478429bfee31e389d99286418008d379345
Victor <victor.monaque@viacesi.fr>
2022-01-28 15:18:29 +0100

    Controller for CreateJobStrategy + comments

--- Skeleton/Appli_V1/Controllers/CreateJobStrategy.cs
+++ Skeleton/Appli_V1/Controllers/CreateJobStrategy.cs
@@ -12,14 +12,19 @@ namespace Appli_V1.Controllers
 
         public void CheckRequirements()
         {
+            // Instanciate new object to work between ExistingJob Model and CreateJobStrategy Controller
             ExistingJob existingJob = new ExistingJob();
-            string contentFile = existingJob.ReadFile();
+            
+            //Instanciate the new object to collect backup's requirement in his attributes
             jobModel newJob = new jobModel();
+
+            // Set up the backup's requirement
             newJob.jobName = "Name";
             newJob.jobType = "complete";
             newJob.sourcePath = "C:/Users/lu-ro/Desktop/Test/Source";
             newJob.targetPath = "C:/Users/lu-ro/Desktop/Test/Destination";
 
+            //Call the Model function to Writes the new backup in the file 
             bool verif = existingJob.WriteExistingJobs(newJob);
 
         }
