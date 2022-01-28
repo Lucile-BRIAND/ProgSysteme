5b1408b35466479ddb5c65509fc422cc3ef533da
Victor <victor.monaque@viacesi.fr>
2022-01-28 16:09:01 +0100

    Need to be fixe : adaptation of View using from RemoveJobStrategy Controller in CreateJobStrategy

--- Skeleton/Appli_V1/Controllers/CreateJobStrategy.cs
+++ Skeleton/Appli_V1/Controllers/CreateJobStrategy.cs
@@ -6,15 +6,15 @@ namespace Appli_V1.Controllers
 {
     class CreateJobStrategy : IStrategyController
     {
-        private int language;
+        private string value_enter;
         //Initialization of language view
         CreateJobStrategyView createJobStrategyView = new CreateJobStrategyView();
 
+         // Instanciate new object to work between ExistingJob Model and CreateJobStrategy Controller
+         ExistingJob existingJob = new ExistingJob();
+
         public void CheckRequirements()
         {
-            // Instanciate new object to work between ExistingJob Model and CreateJobStrategy Controller
-            ExistingJob existingJob = new ExistingJob();
-            
             //Instanciate the new object to collect backup's requirement in his attributes
             jobModel newJob = new jobModel();
 
@@ -25,12 +25,27 @@ namespace Appli_V1.Controllers
             newJob.targetPath = "C:/Users/lu-ro/Desktop/Test/Destination";
 
             //Call the Model function to Writes the new backup in the file 
-            bool verif = existingJob.WriteExistingJobs(newJob);
+            this.value_enter = existingJob.WriteExistingJobs(newJob);
 
         }
         public void CollectExistingData()
         {
-            
+            if(existingJob.WriteExistingJobs(this.value_enter)) //Check if job is added 
+            {
+                    //DisplayValidation
+            }
+            else
+            {
+               createJobStrategyView.DisplayErrorMessage(Singleton_Lang.ReadFile().Error_Create); //Show error message
+            }
+        }
+
+        public void InitView()
+        {
+            createJobStrategyView.DisplayExistingData(Singleton_Lang.ReadFile().Create); //Show added option message
+            CheckRequirements(); //Show all backups 
+            this.value_enter = createJobStrategyView.CollectOptions(); //Collect entry name 
+         
         }
     }
 }
