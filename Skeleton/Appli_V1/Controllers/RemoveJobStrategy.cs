a79b8e6ec70da34105644bfbcf4226f5d92c0f8f
danykhdr <danykhadhar@gmail.com>
2022-01-28 15:50:46 +0100

    RemoveJobStrategy class update

--- Skeleton/Appli_V1/Controllers/RemoveJobStrategy.cs
+++ Skeleton/Appli_V1/Controllers/RemoveJobStrategy.cs
@@ -7,14 +7,33 @@ namespace Appli_V1.Controllers
     class RemoveJobStrategy : IStrategyController
     {
         private int language;
+        private string value_enter;
         RemoveJobStrategyView removeJobStrategyView = new RemoveJobStrategyView();
+        ExistingJob existingJob = new ExistingJob();
+        LanguageFile Singleton_Lang = LanguageFile.GetInstance;
         public void CheckRequirements()
         {
+            string allFile = existingJob.ReadFile(); //Get file content
+            removeJobStrategyView.DisplayExistingData(allFile); //Show file content
 
         }
         public void CollectExistingData()
         {
-
+           if(existingJob.RemoveExistingJobs(this.value_enter)) //Check if job is removed 
+            {
+                    //DisplayValidation
+            }
+            else
+            {
+                removeJobStrategyView.DisplayErrorMessage(Singleton_Lang.ReadFile().Error_Remove); //Show error message
+            }
+        }
+        public void InitView()
+        {
+            removeJobStrategyView.DisplayExistingData(Singleton_Lang.ReadFile().Delete); //Show delete option message
+            CheckRequirements(); //Show all backups 
+            this.value_enter = removeJobStrategyView.CollectOptions(); //Collect entry name 
+         
         }
     }
 }
