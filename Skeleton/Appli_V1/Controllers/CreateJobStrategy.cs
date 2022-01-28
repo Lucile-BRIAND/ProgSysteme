27c0b052ca7ca0d655c2c47c84006a7174fed745
Lucile-BRIAND <lucilebriand.pro@gmail.com>
2022-01-28 15:46:07 +0100

    MERGE: Victor's CreateJob and ExistingJob updates

--- Skeleton/Appli_V1/Controllers/CreateJobStrategy.cs
+++ Skeleton/Appli_V1/Controllers/CreateJobStrategy.cs
@@ -7,14 +7,32 @@ namespace Appli_V1.Controllers
     class CreateJobStrategy : IStrategyController
     {
         private int language;
+
+        //Initialization of language view
         CreateJobStrategyView createJobStrategyView = new CreateJobStrategyView();
+
         public void CheckRequirements()
         {
-            
+            //Instanciates new object to work between ExistingJob Model and CreateJobStrategy Controller
+            ExistingJob existingJob = new ExistingJob();
+
+            //Instanciates the new object to collect backup requirements in his attributes
+            jobModel newJob = new jobModel();
+
+            //Sets the backup requirements
+            newJob.jobName = "Name";
+            newJob.jobType = "complete";
+            newJob.sourcePath = "C:/Users/lu-ro/Desktop/Test/Source";
+            newJob.targetPath = "C:/Users/lu-ro/Desktop/Test/Destination";
+
+            //Calls the Model function to Write the new backup in the file 
+            bool verif = existingJob.WriteExistingJobs(newJob);
+
         }
+
         public void CollectExistingData()
         {
-            
+
         }
     }
 }
