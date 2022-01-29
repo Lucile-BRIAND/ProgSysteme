ec1e05fd1c75f0e987edc9114138b58d98dcec76
Victor <victor.monaque@viacesi.fr>
2022-01-29 12:18:05 +0100

    Adding comments to the main page, Program.cs

--- Skeleton/Appli_V1/Program.cs
+++ Skeleton/Appli_V1/Program.cs
@@ -11,36 +11,40 @@ namespace Appli_V1.Controllers
 {
     class Program : MainController
     {
-        /*public class Employee
+
+        // /!\ VERIFY THE USE OF THIS CLASS BELLOW !
+        public class Employee
         {
             public string Main { get; set; }
 
-        }*/
+        }
 
+        // /!\ VERIFY THE USE OF THIS OBJECT BELLOW !
         MainController mainController = new MainController();
-
+        
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
             /*StreamReader streamreader = new StreamReader("../../../Languages/English_Lang.json");
             string jsonread = streamreader.ReadToEnd();
             Employee employee = JsonConvert.DeserializeObject<Employee>(jsonread);
             Console.WriteLine(employee.Main);*/
 
-            //ChooseLanguageStrategy Premierepage = new ChooseLanguageStrategy();
-            //Premierepage.InitView();
-            //Premierepage.CheckRequirements();
-            //Premierepage.CollectExistingData();
-            //MainController Second_Page = new MainController();
-            //Second_Page.MainMenu();
 
-            CreateJobStrategy createJobStrategyController = new CreateJobStrategy();
+            // Instanciate object to launch the first page, this object will call the InitView to ask the user which language he wants to use.
+            // Then our Controller will collect the user's choice
 
-            createJobStrategyController.CheckRequirements();
+            ChooseLanguageStrategy First_Page = new ChooseLanguageStrategy();
+            First_Page.InitView();
+            First_Page.CheckRequirements();
+            First_Page.CollectExistingData();
 
+            // Now the user's chosen language is set, we can now enter our menu
+            // we instanciate an object from the MainController Class to display the main menu, calling the corresponding method
+            MainController Second_Page = new MainController();
+            Second_Page.MainMenu();
 
         }
-
 
     }
 }
