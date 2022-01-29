e3da483a6c029c3ea3966031dcc0dd97c03eb65c
Victor <victor.monaque@viacesi.fr>
2022-01-29 12:53:31 +0100

    ExecuteJobStrategy class comments and annotated for future removes / updates

--- Skeleton/Appli_V1/Controllers/ExecuteJobStrategy.cs
+++ Skeleton/Appli_V1/Controllers/ExecuteJobStrategy.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.IO;
-
+using Newtonsoft.Json;
+using System.Collections.Generic;
 namespace Appli_V1.Controllers
 {
     class ExecuteJobStrategy //: IStrategyController
     {
+        // Initialize private class attribute
         private int language;
         private string[] jobRequirements;
         private string source;
         private string destination;
         private string name;
         private string type;
+        private string value_enter;
 
+        // The ExecuteJobStrategy class uses specific View class to interact properly with the user
+        // Here we instanciate the View Class object so that we could later call the appropriate method
+        ExecuteStrategyView executeStrategyView = new ExecuteStrategyView();
+
+        // To match with the user's demand, we collect his chosen language
+        LanguageFile Singleton_Lang = LanguageFile.GetInstance;
+
+        // Instanciate object that we need to use below
+        jobModel jobmodel = new jobModel();
+        ExistingJob existingJob = new ExistingJob();
+
+        // /!\ VERIFY THE USE OF THIS METHOD BELOW !
         public ExecuteJobStrategy()
         {
 
@@ -19,17 +34,26 @@ namespace Appli_V1.Controllers
 
         public void CheckRequirements()
         {
-            string[] jobRequirements = new string[] { "test", "complete", "C:/Users/lu-ro/Desktop/Test/Source", "C:/Users/lu-ro/Desktop/Test/Destination" };
+            // In order to execute a backup / job task we need to consider existing job that has already been created
+            var contentFile = System.IO.File.ReadAllText(existingJob.file);
+            var jobModelList = JsonConvert.DeserializeObject<List<jobModel>>(contentFile);
+            foreach (jobModel attribute in jobModelList)
+            {
+                // Here we browse existingJob JSON file to pick up every data corresponding to the user's selected job
+                if (attribute.jobName == this.value_enter)
+                {
+                    name = attribute.jobName;
+                    type = attribute.jobType;
+                    source = attribute.sourcePath;
+                    destination = attribute.targetPath;
+                }
+            }
 
-            name = jobRequirements[0];
-            type = jobRequirements[1];
-            source = jobRequirements[2];
-            destination = jobRequirements[3];
-            int nbfile = 0; //number of files that have been copied
+            int nbfile = 0; // Number of files that have been copied
             StatusLogFile file1 = StatusLogFile.GetInstance;
 
-
-            if (type == "complete")
+            // Here we will chose the right loop, corresponding to the backup's type (complete) and execute the task
+            if (type == "Complete" | type == "Complète")
             {
                 int totalNbFileComplete = Directory.GetFiles(source, "*.*", SearchOption.AllDirectories).Length; //total number of files in the save
 
@@ -51,9 +75,12 @@ namespace Appli_V1.Controllers
                     //Appends the text in the status log file
                     file1.WriteStatusLogMessage(name, type, source, destination, "ACTIVE", totalNbFileComplete, 1000, totalNbFileComplete - nbfile);
                 }
+                // Send Validation Message
+                executeStrategyView.DisplayExistingData(Singleton_Lang.ReadFile().Validation);
 
             }
-            else if (type == "differential")
+            // Here we will chose the right loop, corresponding to the backup's type (differential) and execute the task
+            else if (type == "Differential" | type == "Differentielle")
             {
                 string[] originalFiles = Directory.GetFiles(source, "*", SearchOption.AllDirectories);
                 int totalNbFileDifferential = 0; //number of files that will be copied
@@ -114,16 +141,19 @@ namespace Appli_V1.Controllers
                         file1.WriteStatusLogMessage(name, type, source, destination, "ACTIVE", totalNbFileDifferential, 1000, totalNbFileDifferential - nbfile);
                     }
                 });
+                // Send Validation Message
+                executeStrategyView.DisplayExistingData(Singleton_Lang.ReadFile().Validation);
             }
             else
             {
-                //no refferenced type of save : error()
+                // If there's no refferenced type of save : we display an error message
             }
 
             LogFile lf = LogFile.GetInstance;
             lf.WriteLogMessage(name, source, destination, nbfile, 2);
         }
 
+        // /!\ VERIFY THE USE OF THIS METHOD BELOW !
         public void CollectExistingData()
         {
 
@@ -132,13 +162,9 @@ namespace Appli_V1.Controllers
         public void InitView()
         {
             executeStrategyView.DisplayExistingData(Singleton_Lang.ReadFile().Execute);
-            InitValue();
-        }
-
-        public void InitValue()
-        {
             this.value_enter = executeStrategyView.CollectOptions();
             CheckRequirements();
         }
+
     }
 }
